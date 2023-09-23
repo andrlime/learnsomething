6ec7a73fae104c17ab85b89a0e7032883fb7f642
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-09-22 20:29:35 -0500

    Implement C++ strategy to pass the tests

--- src/hackathonbot.h
+++ src/hackathonbot.h
@@ -3,10 +3,17 @@
 //
 
 #include <vector>
+#include <utility>
 #include "action.h"
 
 #ifndef LEARNSOMETHING_HACKATHONBOT_H
 #define LEARNSOMETHING_HACKATHONBOT_H
+
+enum DELTA_SIGN {
+    POSITIVE = 1,
+    NEGATIVE = -1,
+    ZERO = 0
+};
 
 class HackathonBot {
 public:
@@ -15,6 +22,12 @@ public:
     double getBalance();
     bool isHolding();
 private:
+    double purchasePrice; // 100
+    std::vector<double> prices;
+    std::vector<double> delta; // derivative
+    int in_a_row; // ups and downs in a row
+    int current_sign; // DELTA_SIGN
+    int within_five_percent;
     double balance;
     bool holding;
 };
