657fd2dd325fc363e970fb795ecf65858e7eb1e3
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-09-22 20:49:25 -0500

    the fastest of fast exchanges

--- src/interface.py
+++ src/interface.py
@@ -1,13 +1,17 @@
-class Exchange:
+class ProcrcastinationExchange:
     # implement this!
+    # later
+    balance = 0
+    trades_to_do_later = []
 
     def __init__(self, initialBalance):
         """Initial Balance is the amount that each account should start with."""
-        pass
+        self.balance = initialBalance
+        self.trades_to_do_later = []
 
     def add_trade(self, trade):
         """Adds a trade to the exchange (validation required)
         and returns a match if required. It is up to you on how you will
         handle representing trades. """
-        raise NotImplementedError
+        self.trades_to_do_later.append(trade)
 
